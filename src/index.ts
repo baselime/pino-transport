da2ede855b0f9ae7038842c7c831b3188ddf5f3c
Ankcorn <thomasankcorn@gmail.com>
2023-11-02 11:53:46 +0000

    create pino-http transport

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import build from 'pino-abstract-transport';
+import Axios from 'axios';
+type ClientOpts = {
+    baselimeApiKey: string;
+    service?: string;
+    dataset: string;
+    namespace?: string;
+}
+
+function sendToBaselime(opts: ClientOpts, toSend: any[]) {
+    return Axios.post('https://api.baselime.io/v1', toSend, {
+        headers: {
+            'x-api-key': opts.baselimeApiKey,
+            'x-service': opts.service,
+            'x-dataset': opts.dataset,
+            'x-namespace': opts.namespace,
+        }
+     });
+}
+
+export default function (opts: ClientOpts) {
+  if (!opts?.baselimeApiKey || !opts?.dataset) {
+    throw new Error('Axiom orgId and token are required !');
+  }
+ 
+
+  let toSend: any[] = [];
+  let immediate: null | ReturnType<typeof setImmediate> = null;
+
+  function send() {
+    sendToBaselime(opts, toSend);
+    toSend = [];
+    immediate = null;
+  }
+
+  return build(
+    function (source) {
+      source.on('data', function (obj) {
+        toSend.push(obj);
+        if (!immediate) {
+          immediate = setImmediate(send);
+        }
+      });
+    },
+    {
+      parseLine: (line) => ({ ...JSON.parse(line) }),
+      async close() {
+        if (toSend.length > 0) {
+          clearImmediate(immediate);
+          await sendToBaselime(opts, toSend);
+        }
+      },
+    },
+  );
+};
